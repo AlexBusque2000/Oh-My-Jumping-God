c6f6b94562467b1e6380e9b598f070486132f875
Alex Busqué <alexbusque2000@gmail.com>
2020-01-27 16:52:35 +0100

    HITO 2
    Mecánica del gancho y terreno de prueba

---
+++ Oh My Jumping God/Assets/Scripts/EngancheController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngancheController : MonoBehaviour
+{
+    public Sprite inactiveSprite;
+    public Sprite activeSprite;
+
+    public GameObject player;
+
+    public bool isActive;
+    SpriteRenderer thisRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+
+        if (isActive)
+        thisRenderer.sprite = activeSprite;
+        else thisRenderer.sprite = inactiveSprite;
+    }
+}
---
+++ Oh My Jumping God/Assets/Scripts/GanchoController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GanchoController : MonoBehaviour
+{
+    public float ganchoRange;
+    public float ganchoAreaRange;
+    public float approachSpeed;
+    public float cameraMovementSpeed;
+    public Vector3 cameraOffset;
+
+    GameObject currentGancho;
+
+    Vector3 endPosition;
+    Vector3 startPosition;
+
+    [SerializeField]
+    bool isGanchoing;
+
+    float lerpTime;
+    float cameraLerpTime;
+
+    FirstPersonController firstPersonController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPersonController = GetComponent<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectGancho();
+
+        if (isGanchoing)
+        {
+            firstPersonController.canRotate = false;
+            MoveToGancho();
+            MoveCamera();
+        }
+    }
+
+    void DetectGancho()
+    {
+        RaycastHit hit;
+
+        if (Physics.SphereCast(transform.position, ganchoAreaRange, firstPersonController.PlayerCamera.transform.forward, out hit, ganchoRange))
+        {
+            if (hit.collider.gameObject.GetComponent<EngancheController>())
+            {
+                //Si Detecto otro gancho distinto pero ya tenía uno guardado, desactiva el que tenía guardado
+                if (currentGancho != hit.collider.gameObject && currentGancho != null)
+                    currentGancho.GetComponent<EngancheController>().isActive = false;
+
+
+                currentGancho = hit.collider.gameObject;
+                hit.collider.gameObject.GetComponent<EngancheController>().isActive = true;
+                ActivateGancho();
+            }
+        }
+        else
+        {
+            if (currentGancho != null)
+            currentGancho.GetComponent<EngancheController>().isActive = false;
+        }
+
+    }
+
+    //Esta función solo se activa una vez
+    void ActivateGancho()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            endPosition = currentGancho.transform.GetChild(0).transform.position;
+            startPosition = transform.position;
+            transform.LookAt(new Vector3(endPosition.x, transform.position.y, endPosition.z));
+            firstPersonController.PlayerCamera.transform.localEulerAngles = new Vector3(30, firstPersonController.PlayerCamera.transform.localEulerAngles.y, firstPersonController.PlayerCamera.transform.localEulerAngles.z);
+            lerpTime = 0;
+            cameraLerpTime = 0;
+            isGanchoing = true;
+        }
+    }
+
+    void MoveToGancho()
+    {
+        transform.position = Vector3.Lerp(startPosition, endPosition, lerpTime);
+
+        if (lerpTime < 1)
+        {
+            lerpTime += approachSpeed * Time.deltaTime;
+        }
+        else
+        {
+            firstPersonController.PlayerCamera.transform.localPosition = Vector3.zero;
+            firstPersonController.PlayerCamera.transform.localEulerAngles = Vector3.zero;
+            firstPersonController.canRotate = true;
+            isGanchoing = false;
+        }
+    }
+
+    void MoveCamera()
+    {
+        firstPersonController.PlayerCamera.transform.localPosition = Vector3.Lerp(Vector3.zero, cameraOffset, cameraLerpTime);
+
+        if (lerpTime < 0.5f)
+        {
+            cameraLerpTime += cameraMovementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            cameraLerpTime -= cameraMovementSpeed * Time.deltaTime;
+        }
+    }
+
+    void ClampLerpTimes()
+    {
+        lerpTime = Mathf.Clamp(lerpTime, 0, 1);
+        cameraLerpTime = Mathf.Clamp(cameraLerpTime, 0, 1);
+    }
+}
